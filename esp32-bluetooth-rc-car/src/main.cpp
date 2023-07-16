b50fe6457df1ae91a78f99f25c5e83adb8cf8a93
Christoph Tack <prog.send@gmail.com>
2023-07-16 20:52:23 +0200

    Use ESP-logging Set default speed to maximum

--- esp32-bluetooth-rc-car/src/main.cpp
+++ esp32-bluetooth-rc-car/src/main.cpp
@@ -7,9 +7,11 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+const char *TAG = "Main";
+
 DFRobot_Fermion mainMotor(13, 14);
 DFRobot_Fermion steeringMotor(32, 33);
-int mainMotorSpeed = 25;
+int mainMotorSpeed = 255;
 const int MAX_DUTY_CYCLE = 255;
 const int FLASH_BT_NOT_CONNECTED = 1000;
 const int FLASH_BT_CONNECTED = 250;
@@ -19,25 +21,25 @@ AsyncDelay dead_mans_switch;
 
 typedef enum
 {
-	FORWARD = 70,
-	FORWARD_LEFT = 71,
-	BACKWARDS_LEFT = 72,
-	FORWARD_RIGHT = 73,
-	BACKWARDS_RIGHT = 74,
-	BACKWARDS = 66,
-	STOP = 83,
-	LEFT = 76,
-	RIGHT = 82,
-	SPEED_1 = 49,
-	SPEED_2 = 50,
-	SPEED_3 = 51,
-	SPEED_4 = 52,
-	SPEED_5 = 53,
-	SPEED_6 = 54,
-	SPEED_7 = 55,
-	SPEED_8 = 56,
-	SPEED_9 = 57,
-	SPEED_10 = 113
+	FORWARD = 'F',
+	FORWARD_LEFT = 'G',
+	BACKWARDS_LEFT = 'H',
+	FORWARD_RIGHT = 'I',
+	BACKWARDS_RIGHT = 'J',
+	BACKWARDS = 'B',
+	STOP = 'S',
+	LEFT = 'L',
+	RIGHT = 'R',
+	SPEED_1 = '1',
+	SPEED_2 = '2',
+	SPEED_3 = '3',
+	SPEED_4 = '4',
+	SPEED_5 = '5',
+	SPEED_6 = '6',
+	SPEED_7 = '7',
+	SPEED_8 = '8',
+	SPEED_9 = '9',
+	SPEED_10 = 'q'
 } DIRECTIONS;
 
 BluetoothSerial SerialBT;
@@ -46,7 +48,7 @@ void setup()
 {
 	Serial.begin(115200);
 	SerialBT.begin("RC Car"); // Bluetooth device name
-	Serial.println("The device started, now you can pair it with bluetooth!");
+	ESP_LOGI(TAG, "The device started, now you can pair it with bluetooth!");
 	mainMotor.init(0, 20000, 8);
 	steeringMotor.init(1, 20000, 8);
 	delay_status.start(FLASH_BT_NOT_CONNECTED, AsyncDelay::MILLIS);
@@ -56,6 +58,7 @@ void setup()
 
 void takeAction(int c)
 {
+	ESP_LOGI(TAG, "Received: %c", c);
 	switch (c)
 	{
 	case FORWARD:
@@ -125,7 +128,7 @@ void takeAction(int c)
 		mainMotorSpeed = 250;
 		break;
 	default:
-		Serial.printf("%d ", c);
+		ESP_LOGE(TAG, "Unknown action: %d ", c);
 		break;
 	}
 }
@@ -145,11 +148,13 @@ void loop()
 	}
 	if(dead_mans_switch.isExpired())
 	{
+		ESP_LOGI(TAG, "No signal from controller, stopping car");
 		dead_mans_switch.repeat();
 		takeAction(STOP);
 	}
 	if (delay_status.isExpired())
 	{
+		//Change flash speed depending on connection status
 		delay_status.start(SerialBT.connected() ? FLASH_BT_CONNECTED : FLASH_BT_NOT_CONNECTED, AsyncDelay::MILLIS);
 		digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) == HIGH ? LOW : HIGH);
 	}
