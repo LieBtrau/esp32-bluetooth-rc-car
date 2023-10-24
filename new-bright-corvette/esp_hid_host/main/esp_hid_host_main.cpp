bd2a0d5fc7a4ddb9ff7a75f877fdc3780b4f33d6
Christoph Tack <prog.send@gmail.com>
2023-10-24 20:22:48 +0200

    Adding button functionality

--- new-bright-corvette/esp_hid_host/main/esp_hid_host_main.cpp
+++ new-bright-corvette/esp_hid_host/main/esp_hid_host_main.cpp
@@ -20,6 +20,7 @@
 #include "driver/gpio.h"
 #include "led_indicator.h"
 #include "led_indicator_blink_default.h"
+#include "iot_button.h"
 
 static const char *TAG = "ESP_HIDH_DEMO";
 static QueueHandle_t xSteerQueue = NULL;
@@ -178,6 +179,16 @@ void steering_motor_task(void *pvParameters)
     }
 }
 
+static void button_long_press_cb(void *arg, void *usr_data)
+{
+    ESP_LOGI(TAG, "BUTTON_LONG_PRESS_UP");
+}
+
+static void button_single_click_cb(void *arg, void *usr_data)
+{
+    ESP_LOGI(TAG, "BUTTON_SINGLE_CLICK");
+}
+
 extern "C" void app_main(void)
 {
     esp_err_t ret;
@@ -221,6 +232,32 @@ extern "C" void app_main(void)
     };
     led_indicator_handle_t led_handle = led_indicator_create(&config);
     led_indicator_start(led_handle, BLINK_FACTORY_RESET);
+
+    button_config_t gpio_btn_cfg = {
+        .type = BUTTON_TYPE_GPIO,
+        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
+        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
+        .gpio_button_config = {
+            .gpio_num = PIN_SWITCH,
+            .active_level = 0,
+        },
+    };
+    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
+    if (NULL == gpio_btn)
+    {
+        ESP_LOGE(TAG, "Button create failed");
+    }
+
+    button_event_config_t cfg_long_press_up;
+    cfg_long_press_up.event = BUTTON_LONG_PRESS_UP;
+    cfg_long_press_up.event_data.long_press.press_time = 2000;
+    iot_button_register_event_cb(gpio_btn, cfg_long_press_up, button_long_press_cb, NULL);
+
+    button_event_config_t cfg_single_press_up;
+    cfg_single_press_up.event = BUTTON_SINGLE_CLICK;
+    cfg_single_press_up.event_data.long_press.press_time = 2000;
+    iot_button_register_event_cb(gpio_btn, cfg_single_press_up, button_single_click_cb, NULL);
+
     for (;;)
     {
         // Direction direction;
