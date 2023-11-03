0fc645df7babc9a55f23a750bae576ad01203c6b
Christoph Tack <prog.send@gmail.com>
2023-11-03 21:07:45 +0100

    move Button to other file

--- new-bright-corvette/esp_hid_host/main/esp_hid_host_main.cpp
+++ new-bright-corvette/esp_hid_host/main/esp_hid_host_main.cpp
@@ -15,23 +15,14 @@
 #include "esp_log.h"
 #include "pins.h"
 #include "driver/gpio.h"
-#include "iot_button.h"
+
 #include "LED.h"
 #include "SteerMotor.h"
 #include "ThrustMotor.h"
 #include "BluetoothController.h"
+#include "Button.h"
 
 static const char *TAG = "ESP_HIDH_DEMO";
-
-static void button_long_press_cb(void *arg, void *usr_data)
-{
-    ESP_LOGI(TAG, "BUTTON_LONG_PRESS_UP");
-}
-
-static void button_single_click_cb(void *arg, void *usr_data)
-{
-    ESP_LOGI(TAG, "BUTTON_SINGLE_CLICK");
-}
 
 extern "C" void app_main(void)
 {
@@ -43,31 +34,7 @@ extern "C" void app_main(void)
     thrustMotor.init(PIN_THRUST_MOTOR_A, PIN_THRUST_MOTOR_B);
     BluetoothController btcontroller;
     btcontroller.init();
-
-    button_config_t gpio_btn_cfg = {
-        .type = BUTTON_TYPE_GPIO,
-        .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
-        .short_press_time = CONFIG_BUTTON_SHORT_PRESS_TIME_MS,
-        .gpio_button_config = {
-            .gpio_num = PIN_SWITCH,
-            .active_level = 0,
-        },
-    };
-    button_handle_t gpio_btn = iot_button_create(&gpio_btn_cfg);
-    if (NULL == gpio_btn)
-    {
-        ESP_LOGE(TAG, "Button create failed");
-    }
-
-    button_event_config_t cfg_long_press_up;
-    cfg_long_press_up.event = BUTTON_LONG_PRESS_UP;
-    cfg_long_press_up.event_data.long_press.press_time = 2000;
-    iot_button_register_event_cb(gpio_btn, cfg_long_press_up, button_long_press_cb, NULL);
-
-    button_event_config_t cfg_single_press_up;
-    cfg_single_press_up.event = BUTTON_SINGLE_CLICK;
-    cfg_single_press_up.event_data.long_press.press_time = 2000;
-    iot_button_register_event_cb(gpio_btn, cfg_single_press_up, button_single_click_cb, NULL);
+    initButton(PIN_SWITCH);
 
     LED led;
     led.init(PIN_LED);
---
+++ new-bright-corvette/esp_hid_host/main/include/Button.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void initButton(uint32_t pinButton);
+
+#ifdef __cplusplus
+}
+#endif
